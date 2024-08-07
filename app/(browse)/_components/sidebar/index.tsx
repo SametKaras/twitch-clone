3087177e06ae3bf9f882b3e353862e0435ac205e
SametKaras <sametkaras.tr@gmail.com>
2024-08-07 16:54:54 +0300

    feat: sidebar added

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,3 +1,10 @@
+import { Toggle } from "./toggle";
+import { Wrapper } from "./wrapper";
+
 export const Sidebar = () => {
-  return <div>Sidebar!</div>;
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
 };
